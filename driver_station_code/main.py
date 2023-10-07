de7d481fe989d6577e97813ca57bec135bda42e2
Doug Blanding <dblanding@gmail.com>
2023-10-07 05:17:42 -0400

    minor renaming to be more descriptive

--- driver_station_code/main.py
+++ driver_station_code/main.py
@@ -6,13 +6,14 @@ from time import sleep
 uart = UART(0, 9600, timeout=100)
 uart.write("Hello from HC-06\n")
 
-xAxis = ADC(Pin(26))
-yAxis = ADC(Pin(27))
+x_joy = ADC(Pin(26))
+y_joy = ADC(Pin(27))
 
 while True:
-    xRef = xAxis.read_u16()
-    yRef = yAxis.read_u16()
-    msg = str(xRef) + ', ' + str(yRef)
+    x_val = x_joy.read_u16()
+    y_val = y_joy.read_u16()
+    joy_vals = str(x_val) + ', ' + str(y_val)
+    print(joy_vals)
     if uart.any() > 0:
         str_pose = uart.readline().decode().strip()
         try:
@@ -23,8 +24,7 @@ while True:
         except Exception as e:
             print(e)
             
-        msg = str(xRef) + ', ' + str(yRef)
-        msg += '\n'
-        uart.write(msg)
+        joy_vals += '\n'
+        uart.write(joy_vals)
     
-    sleep(0.2)
+    sleep(0.1)
