5ec92cee5bb84e18d41352698500c03054f53dd5
Doug Blanding <dblanding@gmail.com>
2023-10-19 09:21:35 -0400

    data saving initiated by a button push event

--- controller_code/main.py
+++ controller_code/main.py
@@ -1,23 +1,19 @@
 # driver_station (controller)
 """
 Controller (this code) sends joystick values to robot.
-It will wait patiently until robot is turned on.
-
-Once it is turned on, the robot will respond to received values
-by sending a comma-separated list of data (pose, dist, yaw).
-
-The controller saves the data, then sends more joystick values.
-
-This continues as long as the robot is powered.
-
-To save the data to file, turn the robot off.
-Once the robot is switched off, the controller will wait 2s,
-then rotate the logs and store current data as log0.txt
+Once the robot is turned on, it will respond to received values
+by sending a comma-separated list of data (pose, distances, yaw).
+The controller saves the data (transiently), then sends more joystick values.
+To save the transient data to log a file, push the button.
 """
 
 from machine import ADC, Pin, UART
 import os
 from time import sleep
+import utime
+
+# set up pins
+led = Pin("LED", Pin.OUT)
 
 uart = UART(0, 9600, timeout=100)
 uart.write("Hello from HC-06\n")
@@ -25,71 +21,72 @@ uart.write("Hello from HC-06\n")
 x_joy = ADC(Pin(26))
 y_joy = ADC(Pin(27))
 
-def rotate_logs():
-    # rotate logs and log current data
+button = Pin(18, Pin.IN, Pin.PULL_UP)
+
+# flag signaling it's time to log data
+LOG_DATA = False
+
+
+def get_timestamp():
+    now = utime.localtime()
+    ts = (now[0], now[1], now[2], now[3], now[4], now[5]) 
+    return "{:02d}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}".format(*ts)
+
+
+def data_init():
+    global datafile, f, prev_data
+    datafile = "data.txt"
+    f = open(datafile, 'w')
+    prev_data = ''
+
+
+def button_handler(pin):
+    global LOG_DATA
+    button.irq(handler=None)
+    print(pin)
+    LOG_DATA = True
+
+
+def log_data():
+    global LOG_DATA
+    # move datafile to logfile
+    f.close()
     filenames = os.listdir()
-    print(filenames)
-    if 'log1.txt' in filenames:
-        os.rename('log1.txt', 'log2.txt')
-    if 'log0.txt' in filenames:
-        os.rename('log0.txt', 'log1.txt')
-    with open('log0.txt', 'w') as f:
-        for line in datalist:
-            f.write(line)
+    logfilename = 'log' + get_timestamp() + '.txt'
+    if datafile in filenames:
+        os.rename(datafile, logfilename)
+    
+    LOG_DATA = False
+    data_init()
 
-# Create a list for data
-datalist = []
-prev_data = ''
 
-# initial values associated with saving & logging data
-
-LOG_WAIT_COUNT = 20  # Trigger logging after 2s nonresponse from robot
-robot_alive = False
-logs_rotated = True
-wait_counter = 0
+button.irq(trigger=Pin.IRQ_FALLING, handler=button_handler)
+data_init()
 
 while True:
     x_val = x_joy.read_u16()
     y_val = y_joy.read_u16()
     joy_vals = str(x_val) + ', ' + str(y_val)
-    # print(joy_vals)
 
     if uart.any() > 0:
-        # normal data exchange with robot (until robot is turned off)
-        robot_alive = True
-        logs_rotated = False
+        # normal data exchange with robot
+
         # read incoming data
         str_data = uart.readline().decode()
         print(str_data.rstrip())
 
-        # append line of data to file
+        # write to datafile
         if not str_data == prev_data:
             prev_data = str_data
-            datalist.append(str_data)
+            f.write(str_data)
+            f.flush()
 
         # Send joy_vals to robot
         joy_vals += '\n'
         uart.write(joy_vals)
 
-    elif robot_alive:
-        # start counter when no data received from robot
-        wait_counter = 0
-        robot_alive = False
-        print("robot_alive")
+    if LOG_DATA:
+        log_data()
 
-    elif wait_counter == LOG_WAIT_COUNT:
-        # Time's up! Log data and rotate logs.
-        rotate_logs()
-        logs_rotated = True
-        wait_counter = 0
-        print("logs rotated")
-
-    elif not logs_rotated:
-        # increment counter (keep waiting)
-        wait_counter += 1
-        print("wait_counter = ", wait_counter)
-
-    else:
-        print("waiting")
-
+    led.toggle()
     sleep(0.1)
